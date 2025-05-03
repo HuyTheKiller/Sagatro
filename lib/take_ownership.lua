576107e6ad9d82517a47c03d6abe8faea4011a18
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-05-03 20:29:35 +0700

    Oops

--- lib/take_ownership.lua
+++ lib/take_ownership.lua
@@ -2,8 +2,10 @@ SMODS.Booster:take_ownership_by_kind('Buffoon', {
     create_card = function(self, card, i)
         local _card = {set = "Joker", area = G.pack_cards, skip_materialize = true, soulable = true, key_append = "buf"}
         if Sagatro.config.DisableOtherJokers then
-            if G.GAME.saga_event.alice_in_wonderland.little_bill and i == 1 then
+            if G.GAME.saga_event.alice_in_wonderland.white_rabbit_house and i == 1 then
                 _card.key = "j_sgt_unlabeled_bottle"
+            elseif G.GAME.saga_event.alice_in_wonderland.goodbye_frog then
+                
             end
         end
         return _card
