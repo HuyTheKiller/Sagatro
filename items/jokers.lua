8b6cf4bf6d35b2299bba452c7e58ba6931f5fb4b
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-05-29 12:57:40 +0700

    Vrej

--- items/jokers.lua
+++ items/jokers.lua
@@ -1115,6 +1115,7 @@ local mushroom = {
                         G.GAME.saga_event.alice_in_wonderland.red_queen = true
                     end
                     if not G.GAME.saga_event_check.alice_in_wonderland.pig_and_pepper then
+                        if Sagatro.debug then print("mushroom used") end
                         G.GAME.saga_event.alice_in_wonderland.pig_and_pepper = true
                     end
                     if card.ability.extra - 1 <= 0 then
