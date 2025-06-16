fedeb335e54a4db294d7d2f6d7d6606af6ba002f
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-06-16 10:50:29 +0700

    local _card

--- items/jokers.lua
+++ items/jokers.lua
@@ -3556,12 +3556,12 @@ local lamp_genie = {
                     G.CONTROLLER.locks.genie_init = true
                     G.E_MANAGER:add_event(Event({func = function()
                         local key = "p_sgt_wish_primary"
-                        local card = Card(G.play.T.x + G.play.T.w/2 - G.CARD_W*1.27/2,
+                        local _card = Card(G.play.T.x + G.play.T.w/2 - G.CARD_W*1.27/2,
                         G.play.T.y + G.play.T.h/2-G.CARD_H*1.27/2, G.CARD_W*1.27, G.CARD_H*1.27, G.P_CARDS.empty, G.P_CENTERS[key], {bypass_discovery_center = true, bypass_discovery_ui = true})
-                        card.cost = 0
-                        card.from_tag = true
-                        G.FUNCS.use_card({config = {ref_table = card}})
-                        card:start_materialize()
+                        _card.cost = 0
+                        _card.from_tag = true
+                        G.FUNCS.use_card({config = {ref_table = _card}})
+                        _card:start_materialize()
                         G.CONTROLLER.locks.genie_init = nil
                     return true end }))
                 return true end }))
@@ -3572,13 +3572,13 @@ local lamp_genie = {
                     G.CONTROLLER.locks.genie_second_pack = true
                     G.E_MANAGER:add_event(Event({func = function()
                         local key = "p_sgt_wish_secondary"
-                        local card = Card(G.play.T.x + G.play.T.w/2 - G.CARD_W*1.27/2,
+                        local _card = Card(G.play.T.x + G.play.T.w/2 - G.CARD_W*1.27/2,
                         G.play.T.y + G.play.T.h/2-G.CARD_H*1.27/2, G.CARD_W*1.27, G.CARD_H*1.27, G.P_CARDS.empty, G.P_CENTERS[key], {bypass_discovery_center = true, bypass_discovery_ui = true})
-                        card.cost = 0
-                        card.from_tag = true
-                        booster_obj = card.config.center
-                        G.FUNCS.use_card({config = {ref_table = card}})
-                        card:start_materialize()
+                        _card.cost = 0
+                        _card.from_tag = true
+                        booster_obj = _card.config.center
+                        G.FUNCS.use_card({config = {ref_table = _card}})
+                        _card:start_materialize()
                         G.CONTROLLER.locks.genie_second_pack = nil
                     return true end }))
                 return true end }))
