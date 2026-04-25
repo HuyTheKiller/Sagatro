9cb0d03f1119c8f62bf48e4373201cdbf2d30c49
HuyTheKiller <quochuynguyen0901@gmail.com>
2026-04-25 17:05:53 +0700

    Add card attributes

---
+++ lib/attributes.lua
@@ -0,0 +1,11 @@
+if SMODS.Attribute then
+    SMODS.Attribute{key = "emult"}
+    SMODS.Attribute{key = "debuff"}
+    SMODS.Attribute{key = "position"}
+    SMODS.Attribute{key = "shop_slot"}
+    SMODS.Attribute{key = "modify_ante"}
+    SMODS.Attribute{key = "modify_joker"}
+    SMODS.Attribute{key = "modify_weight"}
+    SMODS.Attribute{key = "consumable_slot"}
+    SMODS.Attribute{key = "booster_pack_slot"}
+end
