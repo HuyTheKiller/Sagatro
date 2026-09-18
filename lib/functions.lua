d6d950820a628ddacc872ad55cf8a3f6d8e8fecf
HuyTheKiller <quochuynguyen0901@gmail.com>
2026-09-18 10:20:10 +0700

    Do not call `save_run` here, again 😭

--- lib/functions.lua
+++ lib/functions.lua
@@ -3536,7 +3536,6 @@ function Sagatro.calculate_fish_joker(card, context)
                             context.full_hand[i]:flip()
                             play_sound('tarot2', percent, 0.6)
                             context.full_hand[i]:juice_up(0.3, 0.3)
-                            save_run()
                             return true
                         end
                     }))
